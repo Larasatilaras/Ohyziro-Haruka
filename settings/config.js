24b8d6f91ddf8925d6df9b605b4b883e6793743d
Eziitzy <bbima5181@gmail.com>
2022-03-26 00:03:11 +0700

    tez

---
+++ settings/config.js
@@ -0,0 +1,30 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+// self or public
+global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
+global.anticall = true
+global.anti_delete = false
+
+// setting
+global.ownername ="Ohyziro_𖠌⁩"
+global.ownernumber = "6285758050756"
+global.botname = "Ohyziro_Bot 𖠌⁩"
+global.thumbnail = fs.readFileSync("./settings/Haruka.jpg") //sesuaikan dengan nama foto
+global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
+global.lolkey = 'ohyziroXkey' //register lolhuman.xyz klo mau apikey
+global.zenzkey = 'sanzychan01' //register zenzapi.xyz klo mau apikey
+global.limit = {
+		free:20,
+		premium:1000
+	}
+global.session_name = "session.json"
+
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
